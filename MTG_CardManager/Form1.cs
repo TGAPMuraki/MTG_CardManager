e5ea6a720754b72a2b7ed81a5c353e6949e3aaf6
Merlin Bezrodnow <merlin.bezrodnow@hotmail.de>
2016-08-25 23:53:50 +0200

    New Class: MagicCard

---
+++ MTG_CardManager/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace MTG_CardManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var card = new MagicCard();
+            MessageBox.Show(card.ConvertManaCost("20UU{W/R}{U/B}{2/R}").ToString());
+        }
+
+        private void readHTML()
+        {
+            string urlAddress = "http://google.com";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlAddress);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = null;
+
+                if (response.CharacterSet == null)
+                {
+                    readStream = new StreamReader(receiveStream);
+                }
+                else
+                {
+                    readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                }
+
+                string data = readStream.ReadToEnd();
+
+                response.Close();
+                readStream.Close();
+            }
+        }
+    }
+}
---
+++ MTG_CardManager/MagicCard.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace MTG_CardManager
+{
+    
+    enum Color { Colorless, White, Blue, Black, Red, Green }
+    enum Rarity { BasicLand, Common, Uncommon, Rare, MythicRare, Special }
+
+    class MagicCard
+    {
+        public String name { get; set; }
+        public String ruleText { get; set; }
+        public String flavorText { get; set; }
+        public String[] types { get; set; }
+        public Color[] color { get; set; }
+        public String manaCost { get; set; }
+        public String power { get; set; }
+        public String toughness { get; set; }
+        public int convertedManaCost { get { return ConvertManaCost(manaCost); } }
+        public string type { get; set; }
+        public string edition { get; set; }
+
+        public int ConvertManaCost(String mana)
+        {
+            String pattern = "(\\d+|W|U|B|R|G|{...})";
+            Regex regex = new Regex(pattern, RegexOptions.IgnoreCase);
+            Match match = regex.Match(mana);
+            int manaCost = 0;
+            while(match.Success)
+            {
+                String value = match.Value;
+                if (value.Contains("/"))
+                    value = value.Substring(1, value.IndexOf("/") - 1);
+
+                try
+                {
+                    manaCost += Convert.ToInt32(value);
+                }
+                catch(FormatException error)
+                {
+                    if (!"WUBRG".Contains(value))
+                        throw error;
+                    manaCost++;
+                }
+
+                match = match.NextMatch();
+            }
+            return manaCost;
+        }
+    }
+
+}
